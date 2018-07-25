d899790c3a7d322f2d0e9ac19f6bc19e994e56b6
Quemuel Nassor <quemuelalp@hotmail.com>
2018-07-25 10:42:17 -0300

    adicionais para a instalacao

--- config.bat
+++ config.bat
@@ -1,6 +1,12 @@
-"Dev-Cpp 5.11 TDM-GCC 4.9.2 Setup.exe" /S
-start "C:\Program Files (x86)\Dev-Cpp\devcpp.exe" main_file.cpp
+@echo ________________________________________________________________________________________________________________________  
+@echo Aguarde o fim da instalacao do Dev-C++
+start /b /wait install.bat 
+@echo ________________________________________________________________________________________________________________________  
+@echo Abrindo arquivo README"
 start wordpad README.txt
+@echo ________________________________________________________________________________________________________________________                                    
+@echo Abrindo codigo fonte para compilacao"
+start "C:\Program Files (x86)\Dev-Cpp\devcpp.exe" main_file.cpp 
 exit
 
 
