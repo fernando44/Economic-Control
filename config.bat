7d26da69669eb39af1958e7d479661f6d0ac2d46
Quemuel Nassor <quemuelalp@hotmail.com>
2018-07-25 11:05:05 -0300

    adicao do comando copiar atalho no arquivo config

--- config.bat
+++ config.bat
@@ -1,12 +1,14 @@
 @echo ________________________________________________________________________________________________________________________  
 @echo Aguarde o fim da instalacao do Dev-C++
-start /b /wait install.bat 
+REM start /b /wait install.bat 
 @echo ________________________________________________________________________________________________________________________  
 @echo Abrindo arquivo README"
 start wordpad README.txt
 @echo ________________________________________________________________________________________________________________________                                    
-@echo Abrindo codigo fonte para compilacao"
+@echo Abrindo codigo fonte para compilacao
 start "C:\Program Files (x86)\Dev-Cpp\devcpp.exe" main_file.cpp 
+@echo Copiando atalho para o desktop
+copy CE.lnk %USERPROFILE%\Desktop
 exit
 
 
