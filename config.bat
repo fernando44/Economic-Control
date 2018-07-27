6f0bc8392c4a41128884ed544f597cae8193b086
Quemuel Nassor <quemuelalp@hotmail.com>
2018-07-27 12:00:54 -0300

    correções no link

--- config.bat
+++ config.bat
@@ -2,13 +2,13 @@
 @echo Aguarde o fim da instalacao do Dev-C++
 start /b /wait install.bat 
 @echo ________________________________________________________________________________________________________________________
-@echo Abrindo arquivo README"
+@echo Abrindo arquivo README
 start wordpad README.txt
 @echo ________________________________________________________________________________________________________________________
 @echo Abrindo codigo fonte para compilacao
 start "C:\Program Files (x86)\Dev-Cpp\devcpp.exe" main_file.cpp 
 @echo Copiando atalho para o desktop
-copy CE.lnk %USERPROFILE%\Desktop
+copy CE.lnk "%USERPROFILE%\Desktop"
 exit
 
 
