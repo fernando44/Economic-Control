f4d2dd4123a81c74a808ecca2a4d8ff964f56d36
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2020-03-22 14:52:50 -0300

    Add universal DateTime item into date library

--- date.h
+++ date.h
@@ -1,14 +1,21 @@
+/*
+	This library contains a function to return date 
+	of splitered and integer form
+*/
+
+//Dependencies
 #include<time.h>
 
-//global strings to get date value
+//Global strings to get date value
 char Date[11];
 char Month[11];
 char Year[5];
+char DateTimeNow[20];
 
-//function to get system date
+//Function to get system date
 void getDate(){
 	
-	//struct for get system date
+	//Struct for get system date
 	struct tm * tm;
 	time_t t;
 	time(&t);
@@ -17,5 +24,7 @@ void getDate(){
 	strftime(Date,11,"%d/%m/%Y", tm);
 	strftime(Month,11,"%B",tm);
 	strftime(Year,5,"%Y",tm);
+	strftime(DateTimeNow,20,"%Y-%m-%d %X",tm);
+	
 }
 
