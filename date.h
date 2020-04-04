536f28b27a9782a19acb3edc39007b823ba1737b
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2020-04-04 18:45:16 -0300

    Update date library

--- date.h
+++ date.h
@@ -6,11 +6,15 @@
 //Dependencies
 #include<time.h>
 
-//Global strings to get date value
+//Global variables
 char Date[11];
 char Month[11];
 char Year[5];
 char DateTimeNow[20];
+char Time[10];
+
+//Matrix of months of year
+char MonthsOfYear[12][10] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
 //Function to get system date
 void getDate(){
@@ -21,10 +25,12 @@ void getDate(){
 	time(&t);
 	tm = localtime(&t);
 	
+	//Fills global variables
 	strftime(Date,11,"%d/%m/%Y", tm);
 	strftime(Month,11,"%B",tm);
 	strftime(Year,5,"%Y",tm);
 	strftime(DateTimeNow,20,"%Y-%m-%d %X",tm);
-	
+	strftime(Time,10,"%X",tm);
+
 }
 
