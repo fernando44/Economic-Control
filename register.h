f5f5732eb383741ce4d7e9aa000e00fb9bf87071
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2020-05-31 23:04:06 -0300

    changed atributes from reg

--- register.h
+++ register.h
@@ -10,8 +10,8 @@
 
 //Dinamic data struct for lists
 typedef struct regType{
-	struct regType *ant; 
-	struct regType *prox;
+	struct regType *prev; 
+	struct regType *next;
 	int id; 
 	char description[100];
 	char category[50];	
@@ -31,7 +31,7 @@ void initalizeString(char* String, int Size){
 //Create an empty register
 reg *newReg(){
     reg *emptyReg = (reg *)malloc(sizeof(reg));						//Allocates *reg size memory
-    emptyReg->prox = emptyReg->ant = NULL;							//Initalize poiters with NULL
+    emptyReg->next = emptyReg->prev = NULL;							//Initalize poiters with NULL
     initalizeString(emptyReg->date,20);								//Initialize date with \0
     initalizeString(emptyReg->description,100);						//Initialize description with \0
     initalizeString(emptyReg->category,50);							//Initialize category with \0
@@ -43,45 +43,45 @@ reg *newReg(){
 
 //Returns to the end of the list
 reg *navEnd(reg *regsList){
-	return (regsList->prox != NULL ? navEnd(regsList->prox) : regsList);
+	return (regsList->next != NULL ? navEnd(regsList->next) : regsList);
 };
 
 //Returns to the beginning of the list
 reg *navStart(reg *regsList){
-	return (regsList->ant != NULL ? navStart(regsList->ant) : regsList);
+	return (regsList->prev != NULL ? navStart(regsList->prev) : regsList);
 };
 
 //Add a new item at the beginning of the list
 reg *addStart(reg *regsList, reg *newItem){
 	regsList = navStart(regsList);
-	newItem->ant = regsList->ant;
-	regsList->ant = newItem;
-	newItem->prox = regsList;
+	newItem->prev = regsList->prev;
+	regsList->prev = newItem;
+	newItem->next = regsList;
 	return newItem;
 };
 
 //Add a new item at the middle of the list
 reg *addMiddle(reg *regsList, reg *newItem){
-	newItem->prox = regsList->prox;
-	regsList->prox != NULL ? regsList->prox->ant = newItem : 0;
-	newItem->ant = regsList;
-	regsList->prox = newItem;
+	newItem->next = regsList->next;
+	regsList->next != NULL ? regsList->next->prev = newItem : 0;
+	newItem->prev = regsList;
+	regsList->next = newItem;
 	return navStart(regsList);
 };
 
 //Add a new item to the end of the list
 void addEnd(reg *regsList, reg *newItem){
 	regsList = navEnd(regsList);
-	newItem->ant = regsList;
-	newItem->prox = regsList->prox;
-	regsList->prox = newItem;
+	newItem->prev = regsList;
+	newItem->next = regsList->next;
+	regsList->next = newItem;
 };
 
 //Remove the first item at the beginning of the list
 reg *remStart(reg *regsList){
 	regsList = navStart(regsList);
-	reg *temp = regsList->prox;
-	(temp != NULL) ? (temp->ant = regsList->ant) : 0;
+	reg *temp = regsList->next;
+	(temp != NULL) ? (temp->prev = regsList->prev) : 0;
 	free(regsList);
 	return temp;
 };
@@ -89,9 +89,9 @@ reg *remStart(reg *regsList){
 //Remove an item in the middle of the list
 reg *remMiddle(reg *regsList){
 	reg *temp = navStart(regsList);
-	if(regsList->ant != NULL && regsList->prox != NULL){
-		regsList->ant->prox = regsList->prox;
-		regsList->prox->ant = regsList->ant;
+	if(regsList->prev != NULL && regsList->next != NULL){
+		regsList->prev->next = regsList->next;
+		regsList->next->prev = regsList->prev;
 		free(regsList);
 	}
 	return temp;
@@ -100,6 +100,6 @@ reg *remMiddle(reg *regsList){
 //Remove the last item at the end of the list
 void remEnd(reg *regsList){
 	regsList = navEnd(regsList);
-	(regsList->ant != NULL) ? (regsList->ant->prox = regsList->prox) : 0;
+	(regsList->prev != NULL) ? (regsList->prev->next = regsList->next) : 0;
 	free(regsList);
 };
