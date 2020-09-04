32d25c6132d94ebe4d6557a854a3f8d9f7d59405
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2020-09-04 10:03:07 -0300

    optimized code and refactored directory separator char

--- mainFile.c
+++ mainFile.c
@@ -3,25 +3,34 @@
 	program execution and verification environment
 */
 
-//libraries
+//Dependencies
 #include<stdio.h>
 #include<string.h>
 #include<sys/stat.h>
 
-//Constprevs
+//Constants
 #define LIN 50
 #define COL 50
 
-//Defines '\' or '/' on unix/windows environtments
-const char *unx = "//";
-const char *win = "\\";
+//Funtion to create directories
+void ___mkdir (char *path);
+
+//Defines '\' or '/' and mkdir on unix/windows environtments
 #ifdef __unix__         
     
-	#define BARKEY unx
+	const char *DIRECTORY_SEPARATOR_CHAR = "/";
+	
+	void ___mkdir (char *path){
+		mkdir(path,0700);
+	}
 
 #elif defined(_WIN32) || defined(WIN32) 
 
-   #define BARKEY win
+	const char *DIRECTORY_SEPARATOR_CHAR = "\\";
+	
+	void ___mkdir (char *path){
+   		mkdir(path);
+	}
 
 #endif
 
@@ -30,19 +39,11 @@ const char *win = "\\";
 #include"date.h"
 #include"menu.h"
 
-//Funtion to create directories
-void ___mkdir (char *path){
-	
-	#ifdef __unix__         
-	    
-		mkdir(path,0700);
-	
-	#elif defined(_WIN32) || defined(WIN32) 
-	
-	  	mkdir(path);
-	
-	#endif
-}
+//Global variables 
+char basePath[30] = "Database";
+char yearsPath[25];
+extern DateTime dateTime;
+extern char categoryList[LIN][COL];
 
 //Pointers for files
 FILE *dataBase,*yearList;
@@ -51,80 +52,108 @@ FILE *dataBase,*yearList;
 void checkDatabase(){
 	
 	//Local variables
-	char directory[30] = "Database";
-	strcat(directory,(char *)BARKEY);
-	char yearsPath[30] = "Database";
-	strcat(yearsPath,(char *)BARKEY);
-	strcat(yearsPath,"list_years.txt");
-	char verify[30]; 
+	// char yearsPath[25];
+	char dataBaseFileCheck[30]; 
 	char itemResult[7]; 
-	char year[5];
+	char filePathTxt[30]; 
+	char filePathJson[30]; 
+	
+	//Creating path to list years
+	strcat(basePath,DIRECTORY_SEPARATOR_CHAR);
+	strcpy(yearsPath,basePath);
+	strcat(yearsPath,"list_years.txt");
 	
 	//Call datetime function
 	getDate();
-	strcpy(year,Year);
 	
 	//Creating path to data base
-	strcat(year,(char *)BARKEY);
-	strcat(directory,year);
+	strcat(basePath,dateTime.Year);
+	strcat(basePath,DIRECTORY_SEPARATOR_CHAR);
 	
 	//Creating path to check if a data base exists
-	strcpy(verify,directory);
-	strcat(verify,"file_check.txt");
+	strcpy(dataBaseFileCheck,basePath);
+	strcat(dataBaseFileCheck,"file_check.txt");
 	
 	//Checking if an old data base exists
-	if((dataBase = fopen(verify,"rb")) == NULL){
+	if((dataBase = fopen(dataBaseFileCheck,"rb")) == NULL){
         
 		//Creating directory to data base
 		___mkdir("Database");
-		___mkdir(directory);
+		___mkdir(basePath);
 		
 		//Validating new data base
-		dataBase = fopen(verify,"wb");
+		dataBase = fopen(dataBaseFileCheck,"wb");
 		fprintf(dataBase,"%s","file_check is an internal file, if it is modified or deleted the program may not work correctly");
 		
+		//Close the file
+		fclose(dataBase);
 		
-		//Opening list of years in reading mode
-		yearList = fopen(yearsPath,"r");		
+		//Concatenate current month
+		strcat(basePath,dateTime.Month);
+		strcpy(filePathTxt,basePath);
+		strcpy(filePathJson,basePath);
 		
-		//Search current year in file
-		do{				
-			//Add current year in file if don't exist
-			if(fscanf(yearList,"%s",itemResult) == EOF && strcmp(itemResult,year) != 0){
-				
-				//Open the file in write mode
-				fclose(yearList);
-				yearList = fopen(yearsPath,"a");
-				
-				//Write the current year in the file
-				fprintf(yearList,"%s",year);
-				fprintf(yearList,"%c",'\n');
-			}
-		}while(strcmp(itemResult,"-1") == 0);
+		//Concatenate file extensions
+		strcat(filePathTxt,".txt");
+		strcat(filePathJson,".json");
 		
-		//Close the files
+		//Write new files
+		dataBase = fopen(filePathJson,"w");
+		dataBase = fopen(filePathTxt,"w");
+		
+		//Opening list of years in append mode
+		yearList = fopen(yearsPath,"a");
+		
+		//Write the current year in the file
+			fprintf(yearList,"%s",dateTime.Year);
+			fprintf(yearList,"%c",'\n');
+		
+		//Close the file
 		fclose(yearList);
 		fclose(dataBase);
+		
+	}else{
+		
+		//Concatenate current month
+		strcat(basePath,dateTime.Month);
 	}
+	
+	//Opening list of years in append mode
+	yearList = fopen(yearsPath,"a");		
+	
+	//Search current year in file
+	while(fscanf(yearList,"%s",itemResult) != EOF){
+
+		//Add current year in file if don't exist
+		if(atoi(itemResult) == EOF && strcmp(itemResult,dateTime.Year) != 0){
+			
+			//Write the current year in the file
+			fprintf(yearList,"%s",dateTime.Year);
+			fprintf(yearList,"%c",'\n');
+			
+		}else if (atoi(itemResult) != EOF && strcmp(itemResult,dateTime.Year) == 0){
+			
+			break;
+		}
+		
+	}
+	
+	//Close the files
+	fclose(yearList);
 }
 
 //Main function
 int main(){
 	
-	//Local variables
-	getDate();
-	char path[30] = "Database";
-	strcat(path,(char *)BARKEY);
-	strcat(path,Year);
-	strcat(path,(char *)BARKEY);
-	strcat(path,Month);
-	
 	//Call checks data base function
 	checkDatabase();
-		
+	
+	//Load category list
+	loadConfig(categoryList);
+	
 	//Call menu program function
-	Menu(newReg(),path,dataBase);
+	Menu(newReg(),basePath,dataBase);
 
-	return(EXIT_SUCCESS);
+	return(0);
 }
 
